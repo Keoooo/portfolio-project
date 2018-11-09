341b879bf8cad813a3a7e9fe81c7e16a3e5cec30
Andy Keogh <Andykeoghleic@gmail.com>
2018-11-09 16:41:18 +0000

    added model and amind

--- portfolioAK/settings.py
+++ portfolioAK/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # apps created by me
+    'projects',
+    'article',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
