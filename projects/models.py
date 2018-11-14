e45a3bb03eabebabfd1d7b49145d26d3b6f7bf64
Andy Keogh <Andykeoghleic@gmail.com>
2018-11-14 17:56:00 +0000

    finished homepage

--- projects/models.py
+++ projects/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class PortfolioExample(models.Model):
 	image = models.ImageField(upload_to='images/')
 	title = models.CharField(max_length=100)
+	link = models.CharField(max_length=100)
 	summery = models.TextField()
 
 	def __str__(self):
