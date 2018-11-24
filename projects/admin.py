ef91fd4dfbc7c5d92222ece47a75804058f15395
Andy Keogh <Andykeoghleic@gmail.com>
2018-11-24 21:40:22 +0000

    Added summernote to summery

--- projects/admin.py
+++ projects/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import PortfolioExample
 
+class SomeModelAdmin(SummernoteModelAdmin):  # instead of ModelAdmin
+        summernote_fields = '__all__'
 
-admin.site.register(PortfolioExample)
+admin.site.register(PortfolioExample, SummernoteModelAdmin)
+
